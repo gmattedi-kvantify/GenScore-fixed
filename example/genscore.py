d11a72a42e751ffe65612f51fb90b9beffa0fb53
Giulio Mattedi <145359145+gmattedi-kvantify@users.noreply.github.com>
2023-12-06 11:51:39 +0000

    Update genscore.py
    Do not set BABEL_LIBDIR

--- example/genscore.py
+++ example/genscore.py
@@ -15,7 +15,7 @@ import torch.multiprocessing
 torch.multiprocessing.set_sharing_strategy('file_system')
 
 #you need to set the babel libdir first if you need to generate the pocket
-os.environ["BABEL_LIBDIR"] = "/home/shenchao/.conda/envs/my3/lib/openbabel/3.1.0"
+# os.environ["BABEL_LIBDIR"] = "/home/shenchao/.conda/envs/my3/lib/openbabel/3.1.0"
 
 def Input():
 	p = argparse.ArgumentParser()
